5ad76927aa559e68028744027bf64e574609daf2
Ezequiel Ledesma <ezeledesma97@gmail.com>
2024-09-15 20:48:22 -0300

    Se agregan rutas de carts

---
+++ src/managers/cart-manager.js
@@ -0,0 +1,65 @@
+import * as fs from "fs";
+
+export class CartManager {
+	static ultimoId = 0;
+	constructor(archivo) {
+		if(archivo) {
+			let cargarArchivo = async () => {
+				const resultado = await fs.promises.readFile(archivo, "utf-8");
+				this.carts = JSON.parse(resultado);
+				
+				// Actualizo ultimoId
+				this.carts.map(item => {
+					CartManager.ultimoId = Math.max(CartManager.ultimoId, parseInt(item.id));
+				})
+			}
+			cargarArchivo();
+		}
+		else this.carts = [];
+	}
+
+	addCart(products) {
+		// Validamos los campos obligatorios
+		if(!products){
+			return "Faltan campos obligatorios!";
+		}
+		
+		// Creamos el nuevo carrito:
+		const nuevoCarrito = {
+			id: (++CartManager.ultimoId).toString(),
+			products
+		}
+
+		// Lo guardamos en el array:
+		this.carts.push(nuevoCarrito);
+		return 0;
+	}
+
+	getCarts() {
+		return this.carts;
+	}
+
+	getCartById(id) {
+		const carritoBuscado = this.carts.find(item => item.id === id);
+		if(carritoBuscado) return carritoBuscado.products;
+		else return 0;
+	}
+
+	addProductCart(idCart, idProduct) {
+		const carritoBuscado = this.getCartById(idCart);
+		if(!carritoBuscado) return "No se encontro carrito!";
+		const productoBuscado = carritoBuscado.find(item => item.id === idProduct);
+		if(productoBuscado) {
+			// Si el producto ya existe en el carrito, incremento contador
+			productoBuscado.quantiy++;
+		}
+		else {
+			carritoBuscado.push({
+				id:idProduct,
+				quantiy:1
+			})
+		}
+		return 0;
+	}
+
+}
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,16 +1,46 @@
 import { Router } from "express";
 const router = Router();
-
-// Array para almacenar carts
-const carts = [];
+import { CartManager } from "../managers/cart-manager.js";
+const manager = new CartManager("./src/data/cart.json");
 
 router.get("/api/carts", (req, res) => {
-	res.json(carts);
+	res.json(manager.getCarts());
 })
 
 router.post("/api/carts", (req, res) => {
+	const nuevoCarrito = req.body;
 
+	try {
+		let aux = manager.addCart(nuevoCarrito);
+		if(!aux) res.status(201).send("Producto agregado!");
+		else res.status(409).send(aux);
+	} catch (error) {
+		res.status(500).send("Error del servidor");
+	}
 })
 
+router.get("/api/carts/:cid", (req, res) => {
+	let id = req.params.cid;
+
+	try {
+		const carritoBuscado = manager.getCartById(id);
+		if(carritoBuscado) res.json(carritoBuscado);
+		else res.status(404).send("Carrito no encontrado");
+	} catch (error) {
+		res.status(500).send("Error del servidor");
+	}
+})
+
+router.post("/api/carts/:cid/product/:pid", (req, res) => {
+	let idCart = req.params.cid;
+	let idProduct = req.params.pid;
+	try {
+		let aux = manager.addProductCart(idCart, idProduct);
+		if(!aux) res.status(201).send("Producto agregado en carrito!");
+		else res.status(409).send(aux);
+	} catch (error) {
+		res.status(500).send("Error del servidor");
+	}
+})
 
 export default router;
